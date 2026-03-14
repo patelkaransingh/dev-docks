762aa43c10e1d75aff193884ade880d4691fd86a
patelkaransingh <techcodemac@gmail.com>
2026-03-14 12:39:54 +0530

    feat: initial commit

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default async function Product({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return <div>Product {id}</div>;
+}
